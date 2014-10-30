176ec47decd7052133949dfb945391bb7ff9a689
Alexandra Chifor <Deea@Alexandras-MacBook-Air-4.local>
2014-10-30 16:11:42 +0200

    Removed vagrant, Added todo project

--- vagrant/bootstrap.user.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export DEBIAN_FRONTEND=noninteractive
-
-# Install Ruby latest stable and Rails
-# https://www.digitalocean.com/community/tutorials/how-to-install-ruby-on-rails-on-ubuntu-14-04-using-rvm
-#
-\curl -sSL https://get.rvm.io | bash -s stable --rails
-source ~/.rvm/scripts/rvm
